bf08d2d39a43aea55a7c011d7713ab06de55c16a
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-01-28 02:21:43 -0600

    homepage links to single repo page displaying issues for selected repo

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,6 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -19,6 +20,15 @@ var getRepoIssues = function(repo) {
         }
     });
 };
+
+var getRepoName = function () {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    getRepoIssues(repoName);
+
+    repoNameEl.textContent = repoName;
+}
 
 var displayIssues = function(issues) {
     if (issues.length === 0) {
@@ -59,4 +69,4 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
 }
 
-getRepoIssues("facebook/react");
+getRepoName();
