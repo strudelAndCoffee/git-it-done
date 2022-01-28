99494733b3232e6267917c995e5ec945b1ee27a3
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-01-28 15:14:19 -0600

    added language buttons to filter repos by coding language

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#lauguage-buttons");
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -31,6 +32,19 @@ var getUserRepos = function(user) {
     .catch(function(error) {
         alert("Unable to connect to GitHub");
     });  
+};
+
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert("GitHub user not found");
+        }
+    });
 };
 
 var displayRepos = function(repos, searchTerm) {
@@ -65,4 +79,14 @@ var displayRepos = function(repos, searchTerm) {
     }
 };
 
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+
+    if (language) {
+        getFeaturedRepos(language);
+        repoContainerEl.textContent = "";
+    }
+};
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
